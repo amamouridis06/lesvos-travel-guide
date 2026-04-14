580e3e668ffd0c8c36241b0d8088b1b27603644a
Angel Dev <amamourids@gmail.com>
2026-04-14 18:37:43 +0300

    Update page.tsx

--- app/nature/kolpos-kallonis/page.tsx
+++ app/nature/kolpos-kallonis/page.tsx
@@ -245,10 +245,15 @@ export default function VateraPage() {
             <ul className="space-y-2 text-sm">
               <li>
                 🏘{" "}
-                <Link href="/villages/plomari" className="text-blue-600 hover:underline">
+                <Link href="/villages/kalloni" className="text-blue-600 hover:underline">
+                   Kalloni
+                </Link>
+                 <Link href="/villages/skalla-kallonis" className="text-blue-600 hover:underline">
                  <p> Kalloni, </p>
-                  <p>Skalla Kallonis,</p> 
-                  <p>Agia Paraskeui</p>
+                  Skalla Kallonis
+                </Link>
+                 <Link href="/villages/agia-paraskeui" className="text-blue-600 hover:underline">   
+                   Agia Paraskeui
                 </Link>
               </li>
             </ul>
