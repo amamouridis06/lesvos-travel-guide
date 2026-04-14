65f28406abb678cd357ef695f5a805ed95b31d3d
Angel Dev <amamourids@gmail.com>
2026-04-14 14:12:29 +0300

    Update NavBar.tsx

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -44,7 +44,7 @@ export default function NavBar() {
             </button>
 
             {exploreOpen && (
-              <div className="absolute left-0 mt-2 w-48 bg-black text-white rounded-lg shadow-lg opacity-0 invisible group-hover:opacity-100 group-hover:visible transition duration-200 z-50">
+               <div className="absolute top-8 left-0 bg-black rounded-lg py-3 w-48 shadow-lg">
                 <Link
                   href="/beaches"
                   className="block px-4 py-2 hover:bg-white/10"
