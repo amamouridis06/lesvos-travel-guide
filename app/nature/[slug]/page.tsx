2c00892e2843bd0ab5424f525197702f9d81748e
Angel Dev <amamourids@gmail.com>
2026-04-14 17:17:33 +0300

    Update page.tsx

--- app/nature/[slug]/page.tsx
+++ app/nature/[slug]/page.tsx
@@ -1,32 +1,32 @@
 import Link from "next/link";
 
-export default function BeachesPage() {
-  const beaches = [
-    {
-      slug: "vatera",
-      name: "Βατερά",
-      img: "/vatera.jpg",
-      desc: "Αμμώδης παραλία μήκους 8 km."
-    },
-    {
-      slug: "agios-isidoros",
-      name: "Άγιος Ισίδωρος",
-      img: "/agios-isidoros.jpg",
-      desc: "Διάσημη παραλία με γαλάζια σημαία."
-    }
-  ];
+export default function NarurePage() {
+  // const beaches = [
+  //   {
+  //     slug: "vatera",
+  //     name: "Βατερά",
+  //     img: "/vatera.jpg",
+  //     desc: "Αμμώδης παραλία μήκους 8 km."
+  //   },
+  //   {
+  //     slug: "agios-isidoros",
+  //     name: "Άγιος Ισίδωρος",
+  //     img: "/agios-isidoros.jpg",
+  //     desc: "Διάσημη παραλία με γαλάζια σημαία."
+  //   }
+  // ];
 
   return (
-    <main className="min-h-screen bg-black text-white px-6 pt-32 pb-16 bg-[url('/beach.jpg')] bg-cover bg-center bg-fixed relative">
+    <main className="min-h-screen bg-black text-white px-6 pt-32 pb-16 bg-[url('/flamingo.jpeg')] bg-cover bg-center bg-fixed relative">
 
       <div className="absolute inset-0 bg-black/40 backdrop-blur-[1px]"></div>
 
       <div className="relative z-10">
-        <h1 className="text-4xl font-bold text-center mb-10">Παραλίες Λέσβου</h1>
+        <h1 className="text-4xl font-bold text-center mb-10">The Nature</h1>
 
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 max-w-6xl mx-auto">
           {beaches.map((b, i) => (
-            <Link href={`/beaches/${b.slug}`} key={i}>
+            <Link href={`/nature/${b.slug}`} key={i}>
               <div className="bg-gray-900/80 backdrop-blur-sm rounded-xl border border-gray-700 shadow-xl overflow-hidden hover:scale-[1.03] transition-transform cursor-pointer">
                 
                 <img src={b.img} alt={b.name} className="h-48 w-full object-cover" />
