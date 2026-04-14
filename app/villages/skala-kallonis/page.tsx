3feb99fd70ecc004b429427d7e98fa8e81e25157
Angel Dev <amamourids@gmail.com>
2026-04-15 01:58:43 +0300

    Update page.tsx

--- app/villages/skala-kallonis/page.tsx
+++ app/villages/skala-kallonis/page.tsx
@@ -51,7 +51,22 @@ export default function MolyvosPage() {
             <li> Quiet moments away from noise, offering a peaceful atmosphere ideal for rest and disconnection </li>
           </ul>
         </div>
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3268.7127752453366!2d26.207192726145344!3d39.20675477165979!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14ba92612321ace7%3A0xa00bd2f74c26dd0!2sSkala%20Kallonis!5e1!3m2!1sen!2sgr!4v1776207415541!5m2!1sen!2sgr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+         {/* SIDEBAR */}
+        <aside className="space-y-8">
+
+          {/* Map */}
+          <div className="w-full h-48 rounded-lg overflow-hidden">
+            <iframe 
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3268.7127752453366!2d26.207192726145344!3d39.20675477165979!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14ba92612321ace7%3A0xa00bd2f74c26dd0!2sSkala%20Kallonis!5e1!3m2!1sen!2sgr!4v1776207415541!5m2!1sen!2sgr" 
+                width="100%"
+                height="100%"
+                style={{ border: 0 }}
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+              />
+          </div>
+        </aside>
         {/* BACK BUTTON */}
         {/* <div>
           <a
