1bd62e5eff1d20c78d23670deb8f8fd318f328d8
Angel Dev <amamourids@gmail.com>
2026-04-14 12:32:34 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
               </p>
 
               <Link
-                href="https://lesvos-travel-guide-five.vercel.app/beaches"
+                href="/beaches"
                 className="inline-block mt-6 px-6 py-3 rounded-lg bg-yellow-600 text-white font-semibold hover:bg-yellow-700 transition"
               >
                 Εξερεύνησε το νησί
