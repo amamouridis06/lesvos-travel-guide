bbc247b9e991a6b587bb10478a419e790810503b
Angel Dev <amamourids@gmail.com>
2026-04-14 12:44:33 +0300

    Update page.tsx

--- app/beaches/vatera/page.tsx
+++ app/beaches/vatera/page.tsx
@@ -78,11 +78,6 @@ export default function VateraPage() {
           <div className="w-full h-48 rounded-lg overflow-hidden">
             <iframe
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d52442.62385052624!2d26.179372848547395!3d39.014022754774174!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14baeff2a84e3b1f%3A0x12a7105ca3e69a0a!2sVatera%20Beach!5e1!3m2!1sen!2sgr!4v1776159751071!5m2!1sen!2sgr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"
-                width="100%"
-                height="100%"
-                style={{ border: 0 }}
-                loading="lazy"
-                referrerPolicy="no-referrer-when-downgrade"
             />
           </div>
 
