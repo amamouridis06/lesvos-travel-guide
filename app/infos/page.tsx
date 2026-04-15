a168038eb6d65beabcd27c0718f142529641ba92
Angel Dev <amamourids@gmail.com>
2026-04-15 11:36:59 +0300

    Update page.tsx

--- app/infos/page.tsx
+++ app/infos/page.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 import { Card, CardContent } from "@/components/ui/card";
 // import { MapPin, Phone, Image as ImageIcon, HeartPulse, Search } from "lucide-react";
 
