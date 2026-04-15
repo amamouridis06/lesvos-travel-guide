c4d7c983bf31bcee40245387ac00b4f52d037b75
Angel Dev <amamourids@gmail.com>
2026-04-15 16:42:06 +0300

    Update NavBar.tsx

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -16,7 +16,7 @@ export default function NavBar() {
     isEnglish ? pathname.replace("/en", "") || "/" : `/en${pathname}`;
 
   return (
-    <nav className="fixed top-0 left-0 w-full z-50 bg-blue-600/80 backdrop-blur-md border-b border-blue-500">
+    <nav className="fixed top-0 left-0 w-full z-50 bg-blue-600/60 backdrop-blur-md border-b border-blue-500">
       <div className="max-w-6xl mx-auto px-4 py-8 text-center text-white">
 
         {/* Logo */}
