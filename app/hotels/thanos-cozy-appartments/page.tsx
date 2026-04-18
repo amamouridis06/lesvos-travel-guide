db4870413ac201bcf7ad39a0e2a512d582ecd715
Angel Dev <amamourids@gmail.com>
2026-04-18 21:49:31 +0300

    Update page.tsx

--- app/hotels/thanos-cozy-appartments/page.tsx
+++ app/hotels/thanos-cozy-appartments/page.tsx
@@ -69,9 +69,12 @@ export default function ApartmentPage() {
             </div>
 
             <div className="mt-8 flex flex-col gap-3 sm:flex-row">
-              <button className="rounded-xl bg-orange-500 px-6 py-3 font-medium text-white transition hover:bg-orange-600">
-                Book Now
-              </button>
+              <a
+                  href="https://www.booking.com/hotel/gr/thanos-cozy-apartments.en-gb.html?aid=397594&label=gog235jc-10CAEoggI46AdIM1gDaFyIAQGYATO4ARfIAQzYAQPoAQH4AQGIAgGoAgG4Asejj88GwAIB0gIkOWM2NDNmYTctNGU2ZC00Y2JlLTllMDktM2VkMmViZGY5NTM32AIB4AIB&sid=68f00793dd2d449b60f30b808935e305&checkin=2026-05-11&checkout=2026-05-14&dest_id=14571543&dest_type=hotel&dist=0&group_adults=2&group_children=0&hapos=1&hpos=1&no_rooms=1&req_adults=2&req_children=0&room1=A%2CA&sb_price_type=total&soh=1&sr_order=popularity&srepoch=1776538142&srpvid=ec81844de1470058&type=total&ucfs=1&#no_availability_msg"
+                  className="rounded-xl bg-orange-500 px-6 py-3 font-medium text-white transition hover:bg-orange-600 inline-block text-center"
+              >
+              Book Now
+              </a>
 
              
             </div>
