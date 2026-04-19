a4bf44eca4e2bda8432073b97e566a1ee5f0c19e
Angel Dev <amamourids@gmail.com>
2026-04-20 02:55:37 +0300

    Update page.tsx

--- app/food/tropicana-platanos/page.tsx
+++ app/food/tropicana-platanos/page.tsx
@@ -168,9 +168,6 @@ export default function LimanakiPamfilonPage() {
       <li><strong>Kitchen:</strong> Greek</li>
       <li><strong>Prices:</strong> €€</li>
       <li><strong>Suitable for:</strong> Families, couples, kids and for relax</li>
-      <li><strong>Address:</strong> Plateia Kiriakou, Mithimna 811 08</li>
-      <li><strong>Phone:</strong> 22530 71869</li>
-      <li><strong>Working hours:</strong> From 9:30 to 23:30</li>
     </ul>
   </div>
   <div className="bg-white rounded-2xl shadow-md p-6 border border-gray-100">
