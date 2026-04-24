841ce16ec24ba2b606236d2b61a748af099d13b3
Angel Dev <amamourids@gmail.com>
2026-04-24 12:53:26 +0300

    Update page.tsx

--- app/nature/page.tsx
+++ app/nature/page.tsx
@@ -3,119 +3,42 @@ import Link from "next/link";
 export default function NaturePage() {
   const nature = [
     {
-      slug: "kolpos-kallonis",
-      name: "Kalloni Bay",
+      slug: "forests",
+      name: "Forests",
       img: "/flamingo.jpeg",
-      desc: "A paradise of biodiversity."
+      // desc: "A paradise of biodiversity."
     },
     {
-      slug: "olympos",
-      name: "Mount Olympus (Lesvos)",
+      slug: "geology",
+      name: "Geology",
       img: "/x",
-      desc: "The highest peak of the island (~968m), offering hiking trails, rich biodiversity, and panoramic views."
+      // desc: "The highest peak of the island (~968m), offering hiking trails, rich biodiversity, and panoramic views."
     },
      {
-      slug: "lepetypnos",
-      name: "Lepetymnos Mountain",
+      slug: "mountains",
+      name: "Mountains",
       img: "/x",
-      desc: "A rugged mountain above Molyvos with wild scenery and stunning Aegean views."
+      // desc: "A rugged mountain above Molyvos with wild scenery and stunning Aegean views."
     },
   {
-      slug: "ipsilou",
-      name: "Ipsilou Monastery Area",
+      slug: "thermal",
+      name: "Thermal",
       img: "/x",
-      desc: "A dramatic volcanic landscape with a raw, almost lunar atmosphere—ideal for photography and sunsets.."
+      // desc: "A dramatic volcanic landscape with a raw, almost lunar atmosphere—ideal for photography and sunsets.."
     },
       {
-      slug: "pine-forest",
-      name: "Pine Forests of Gera",
+      slug: "water",
+      name: "Waterfalls",
       img: "/x",
-      desc: "Green pine-covered areas surrounding the Gulf of Gera."
+      // desc: "Green pine-covered areas surrounding the Gulf of Gera."
     },
     {
-      slug: "chestnut-forest",
-      name: "Chestnut Forest of Agiasos",
+      slug: "wetlands",
+      name: "Wetlands",
       img: "/x",
-      desc: "Green pine-covered areas surrounding the Gulf of Gera."
+      // desc: "Green pine-covered areas surrounding the Gulf of Gera."
     },
-    {
-      slug: "potamia",
-      name: "Potamia Valley",
-      img: "/x",
-      desc: "A lush valley with flowing water, plane trees, and peaceful walking paths."
-    },
-    {
-      slug: "evergetoulas",
-      name: "Evergetoulas Valley",
-      img: "/x",
-      desc: "The island’s largest fertile valley, with a river and rich natural life.."
-    },
-    {
-      slug: "salt-pans",
-      name: "Kalloni Salt Pans",
-      img: "/x",
-      desc: "One of Europe’s most important wetlands, home to flamingos and migratory birds."
-    },
-    {
-      slug: "achladeri-wetlands",
-      name: "Achladeri Wetlands",
-      img: "/x",
-      desc: "A quieter but ecologically rich habitat for birdlife and wildlife."
-    },
-    {
-      slug: "napi-vallet",
-      name: "Napi Valley",
-      img: "/x",
-      desc: "Known for wildflowers, butterflies, and untouched natural beauty."
-    },
-    {
-      slug: "petrified-forest",
-      name: "Petrified Forest of Lesvos",
-      img: "/x",
-      desc: "One of Europe’s most important wetlands, home to flamingos and migratory birds."
-    },
-     {
-      slug: "geopark",
-      name: "Lesvos UNESCO Global Geopark",
-      img: "/x",
-      desc: "A UNESCO-recognized geopark combining geology, volcanic history, and rare landscapes."
-    },
-     {
-      slug: "eft-hot-springs",
-      name: "Eftalou Hot Springs",
-      img: "/x",
-      desc: "Natural hot springs set in a wild coastal environment."
-    },
-     {
-      slug: "pol-hot-springs",
-      name: "Polichnitos Hot Springs",
-      img: "/x",
-      desc: "Among the hottest thermal springs in Europe."
-    },
-     {
-      slug: "loutra-therma",
-      name: "Loutra Thermis Springs",
-      img: "/x",
-      desc: "Historic thermal baths in a peaceful natural setting."
-    },
-    {
-      slug: "loutra-therma",
-      name: "Loutra Thermis Springs",
-      img: "/x",
-      desc: "Historic thermal baths in a peaceful natural setting."
-    },
-    {
-      slug: "man-katsa",
-      name: "Man' Katsa Waterfall",
-      img: "/x",
-      desc: "A small seasonal waterfall (best in spring), considered a hidden gem."
-    },
-    {
-      slug: "potamia-streams",
-      name: "Potamia Streams",
-      img: "/x",
-      desc: "Flowing streams that create small cascades during winter and spring."
-    },
+    
   ];
 
 return (
@@ -135,7 +58,7 @@ return (
 
                 <div className="p-5">
                   <h2 className="text-2xl font-semibold mb-2">{b.name}</h2>
-                  <p className="text-gray-300 text-sm">{b.desc}</p>
+                  {/* <p className="text-gray-300 text-sm">{b.desc}</p> */}
                 </div>
 
               </div>
