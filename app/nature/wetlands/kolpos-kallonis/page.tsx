14e31f49a87dcfbe249cf2bc61353489adc15741
Angel Dev <amamourids@gmail.com>
2026-04-24 13:46:34 +0300

    Update page.tsx

--- app/nature/wetlands/kolpos-kallonis/page.tsx
+++ app/nature/wetlands/kolpos-kallonis/page.tsx
@@ -6,7 +6,7 @@ export const metadata = {
   description:"Agios Isidoros beach in Lesvos is one of the most famous and beautiful beaches of the island, especially near Plomari."
 };
 
-export default function kolposkallonisPage() {
+export default function KolposKallonisPage() {
   return (
     <main className="bg-neutral-100 text-neutral-900">
 
