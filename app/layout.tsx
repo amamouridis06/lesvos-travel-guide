caf8b1c3b8ab135e55ff9ca0bd666615513ce07c
amamouridis06 <amamouridis06@gmail.com>
2026-04-28 15:17:48 +0300

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="bg-black text-white">
-        {/*<NavBar />*/}
+        <NavBar />
         <div className="pt-20">{children}</div>
         {/*<Footer />*/}
       </body>
