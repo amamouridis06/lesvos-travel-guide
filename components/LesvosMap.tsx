596d1347c16cd290a58d74df74176795567fe997
amamouridis06 <amamouridis06@gmail.com>
2026-06-13 22:34:28 +0300

    commit

--- components/LesvosMap.tsx
+++ components/LesvosMap.tsx
@@ -269,7 +269,7 @@ const lesvosPlaces: LesvosPlace[] = [
         id: 20,
         name: "Nisi Restaurant",
         location: "Plateia Kiriakou,811 08 Molyvos, Lesvos",
-        category: "Food",
+        category: "Food & Drinks",
         description:
             "Authentic Greek flavors, and fantastic hospitality.",
         coordinates: [39.3693073,26.1733986],
